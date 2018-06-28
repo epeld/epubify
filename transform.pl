638229a4601b0d3a49588a4778863939c5be8fe8
Erik Peldan <erik.peldan@gmail.com>
2018-06-28 22:19:29 +0200

    Move exported goal to top of file

--- transform.pl
+++ transform.pl
@@ -1,16 +1,5 @@
 :- module(transform, [transform/2]).
 
-%
-% Perform transformation rules recursively
-% until no more transformations apply
-apply_rule(Element, Out) :-
-  (myrule(Element, Out0), Out0 \= Element) *->
-    (
-      Out = success(Out0)
-    )
-  ; failure(Element) = Out.
-
-
 transform(Xml, Out) :- 
   maplist(apply_rule, Xml, Out0),
   exclude(=(success(removed)),Out0, Out1),
@@ -21,7 +10,19 @@ transform(Xml, Out) :-
     )*-> (!, transform(Out2, Out))
   ; Out2 = Out
   ).
-  
+
+
+%
+% Perform transformation rules recursively
+% until no more transformations apply
+apply_rule(Element, Out) :-
+  (myrule(Element, Out0), Out0 \= Element) *->
+    (
+      Out = success(Out0)
+    )
+  ; failure(Element) = Out.
+
+ 
 get_element(success(R), R).
 get_element(failure(R), R).
 
