991a015a2312e8a0d7179b39aa0516090e89b863
Erik Peldan <erik.peldan@gmail.com>
2018-06-29 14:04:41 +0200

    Use font module

--- transform.pl
+++ transform.pl
@@ -1,5 +1,6 @@
 :- module(transform, [transform/2]).
 :- use_module(xml, [xml_rule/2]).
+:- use_module(font, [font_rule/3]).
 
 transform(Xml, Out) :- 
   maplist(apply_rule, Xml, Out0),
@@ -32,19 +33,7 @@ get_element(failure(R), R).
 % Transformation Rules
 %
 
-
-% tarpit.pdf
-font_rule(name='CMTI10', Children, italic(Children)).
-font_rule(name='CMBX12', Children, heading(2, Children)).
-
-% halmos.pdf
-font_rule(name='CMBX10', Children, heading(1, Children)).
-font_rule(name='CMCSC10', Children, heading(2, Children)).
-font_rule(name='CMR8', Children, subtitle(Children)).
-font_rule(name='CMR10', Children, body(Children)).
-font_rule(name='CMR7', Children, footer(Children)).
-
-
+% TODO reconsider this rule?
 % font_rule invocation
 myrule(element(font, A, Text), Out) :-
   font_rule(Name, Out0, Out),
