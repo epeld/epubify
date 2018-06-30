f7ea54330a8a30e548ecff01f3ce4ede3664299b
Erik Peldan <erik.peldan@gmail.com>
2018-06-30 17:23:54 +0200

    Make sure load.pl loads all modules now that we use multifile

--- load.pl
+++ load.pl
@@ -2,7 +2,9 @@
 :- use_module(stext, []).
 :- use_module(document, []).
 % :- use_module(block, []).
+:- use_module(char, []).
 :- use_module(font, []).
+:- use_module(transform, []).
 
 :- run_tests.
 
