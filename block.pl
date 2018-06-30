cd77ecb83eb866a2b7c75f6b44b28db43b529af4
Erik Peldan <erik.peldan@gmail.com>
2018-06-30 13:56:37 +0200

    Block-module defines how to join blocks

--- block.pl
+++ block.pl
@@ -1,9 +1,30 @@
 :- module(block,
           [
             block_rule/2,
+            is_paragraphed_block/1,
+            join_blocks/3
           ]).
 :- use_module(attribute, [attribute_tag/3]).
-:- use_module(paragraph, [lines_to_paragraphs/2]).
+:- use_module(paragraph, [lines_to_paragraphs/2, join_paragraphs/3]).
+:- use_module(list, [all_but_last/3]).
+
+join_blocks(
+  element(block, A, B),
+  element(block, A2, B2),
+  element(block, AJoined, BJoined)
+) :-
+  is_paragraphed_block(element(block, A, B)),
+  is_paragraphed_block(element(block, A2, B2)),
+
+  % We will keep all children
+  % except possibly the last in B and first in B2
+  % which might have to be joined
+  all_but_last(B, BAllButLast, BLast),
+  B2 = [B2First | B2AllButFirst],
+  join_paragraphs(B, B2, BB2),
+
+  append([BAllButLast, BB2, B2AllButFirst], Joined).
+  
 
 block_rule(
   element(block, Attrs, Children),
@@ -14,6 +35,8 @@ block_rule(
   assert_all(is_line, Children),
 
   lines_to_paragraphs(Children, Transformed).
+
+
 
 
 % TODO move to line.pl?
