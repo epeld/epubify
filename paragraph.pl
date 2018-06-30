6257946f53037535a5cda9ac2a87dd0c31dcd110
Erik Peldan <erik.peldan@gmail.com>
2018-06-30 11:02:15 +0200

    Remove LeftMargin-arg

--- paragraph.pl
+++ paragraph.pl
@@ -5,21 +5,11 @@
 % :- use_module(attribute, [attribute_tag/3]).
 
 
-
-
-%
-
-%
-
-
-
 lines_to_paragraphs(
-  LeftMargin,
   Lines,
   Paragraphs
 ) :-
   lines_to_paragraphs(
-    LeftMargin,
     none,
     Lines,
     [],
@@ -30,7 +20,6 @@ lines_to_paragraphs(
 % if a line is indented more than the previous
 % then it is the start of a paragraph
 lines_to_paragraphs(
-  LeftMargin,
   PrevLine,
   [Line | Rest],
   Paragraphs,
@@ -40,7 +29,6 @@ lines_to_paragraphs(
   line_bbox(Line, X),
   X0 < X,
   lines_to_paragraphs(
-    LeftMargin,
     Line,
     Rest,
     [paragraph([Line]) | Paragraphs],
@@ -50,7 +38,6 @@ lines_to_paragraphs(
 % if a line is indented the same as the previous
 % then it is the same paragraph
 lines_to_paragraphs(
-  LeftMargin,
   PrevLine,
   [Line | Rest],
   [paragraph(Lines) | Paragraphs],
@@ -60,35 +47,23 @@ lines_to_paragraphs(
   line_bbox(Line, X),
   X =< X0,
   lines_to_paragraphs(
-    LeftMargin,
     Line,
     Rest,
     [paragraph([Line | Lines]) | Paragraphs],
     ParagraphsOut
   ).
-    
 
-% TODO remove this rule and use the block's LeftMargin instead
-% if the initial line in a block is indented same
-% as the next, then it is a continued paragraph
-% from previous block
+
 lines_to_paragraphs(
-  LeftMargin,
   none,
-  [Line, NextLine | Rest],
+  [Line | Rest],
   [],
   ParagraphsOut
 ) :-
-  line_bbox(Line, X0),
-  line_bbox(NextLine, X),
-  X = X0,
   lines_to_paragraphs(
-    LeftMargin,
-    NextLine,
+    Line,
     Rest,
-    [paragraph([NextLine, Line, continued])],
+    [paragraph([Line])],
     ParagraphsOut
   ).
-
-
     
