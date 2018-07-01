381fe271ff040d276b7282dad65751c9ff2c1240
Erik Peldan <erik.peldan@gmail.com>
2018-07-01 17:23:06 +0200

    Identify footnote references

--- font.pl
+++ font.pl
@@ -31,7 +31,7 @@ font_name_style('CMBX12', heading(2)).
 % halmos.pdf
 font_name_style('CMBX10', heading(1)).
 font_name_style('CMCSC10', heading(2)).
-font_name_style('CMR8', subtitle).
+font_name_style('CMR8', footnote(reference)).
 font_name_style('CMR10', regular).
 font_name_style('CMR7', footer).
 font_name_style('CMR6', footnote(reference)).
